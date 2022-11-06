9428d228fce9c5e4e5157ec9e43f6e26b2906085
vistar <1055311345@qq.com>
2022-11-06 22:09:10 +0800

    QDialogButtonBox

--- code/2.Buttons/6.dialogButtonBox/dialogButtonBox/widget.cpp
+++ code/2.Buttons/6.dialogButtonBox/dialogButtonBox/widget.cpp
@@ -6,6 +6,18 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    QDialogButtonBox* buttonBox1 = new QDialogButtonBox(Qt::Orientation::Horizontal, this);
+    buttonBox1->setGeometry(QRect(100, 130, 166, 25));
+    buttonBox1->setStandardButtons(QDialogButtonBox::Yes|QDialogButtonBox::No);
+
+    QDialogButtonBox* buttonBox2 = new QDialogButtonBox(QDialogButtonBox::Close|QDialogButtonBox::Open, this);
+    buttonBox2->setGeometry(QRect(100, 170, 166, 25));
+
+    QDialogButtonBox* buttonBox3 = new QDialogButtonBox(QDialogButtonBox::Apply|QDialogButtonBox::Help,
+                                                        Qt::Orientation::Horizontal, this);
+    buttonBox3->setGeometry(QRect(100, 210, 166, 25));
+
 }
 
 Widget::~Widget()
