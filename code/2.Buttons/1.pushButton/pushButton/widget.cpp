16722d940bd86a392b8a6b866a4b25ba468f3023
vistar <1055311345@qq.com>
2022-11-21 00:06:07 +0800

    Qt工程基本文件详解

--- code/2.Buttons/1.pushButton/pushButton/widget.cpp
+++ code/2.Buttons/1.pushButton/pushButton/widget.cpp
@@ -7,17 +7,17 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    ui->pushButton->setText("按钮");
-    ui->pushButton->resize(80, 80);
-    ui->pushButton->setStyleSheet("QPushButton {"
-                                  "background-color: green;" // 按钮背景色
-                                  "font: bold 20px;"	// 按钮字体
-                                  "border-width: 1px;"	// 按钮边框线宽
-                                  "border-radius: 40px;" // 按钮边框圆角半径
-                                  "color: white;"      // 按钮文字颜色
-                                  "}");
+//    ui->pushButton->setText("按钮");
+//    ui->pushButton->resize(80, 80);
+//    ui->pushButton->setStyleSheet("QPushButton {"
+//                                  "background-color: green;" // 按钮背景色
+//                                  "font: bold 20px;"	// 按钮字体
+//                                  "border-width: 1px;"	// 按钮边框线宽
+//                                  "border-radius: 40px;" // 按钮边框圆角半径
+//                                  "color: white;"      // 按钮文字颜色
+//                                  "}");
 
-    connect(ui->pushButton, &QPushButton::clicked, this, &Widget::btnClicked);
+//    connect(ui->pushButton, &QPushButton::clicked, this, &Widget::btnClicked);
 }
 
 Widget::~Widget()
