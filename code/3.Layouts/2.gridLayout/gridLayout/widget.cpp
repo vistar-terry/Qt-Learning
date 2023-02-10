ee62b862a126efa9db5e3e78e45cc92df9d53d8a
vistar <1055311345@qq.com>
2023-02-10 23:07:00 +0800

    QGridLayout

--- code/3.Layouts/2.gridLayout/gridLayout/widget.cpp
+++ code/3.Layouts/2.gridLayout/gridLayout/widget.cpp
@@ -1,11 +1,15 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QBoxLayout>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    ui->gridLayout->setSpacing(20);
+    ui->gridLayout->setRowMinimumHeight(1, 100);
 }
 
 Widget::~Widget()
