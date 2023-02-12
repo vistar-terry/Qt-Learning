6708dfd16b7af5aaa321a98a236ac15168319cdb
vistar <1055311345@qq.com>
2023-02-12 16:27:33 +0800

    QBoxLayout

--- code/3.Layouts/2.gridLayout/gridLayout/widget.cpp
+++ code/3.Layouts/2.gridLayout/gridLayout/widget.cpp
@@ -8,8 +8,10 @@ Widget::Widget(QWidget *parent)
 {
     ui->setupUi(this);
 
-    ui->gridLayout->setSpacing(20);
-    ui->gridLayout->setRowMinimumHeight(1, 100);
+    int row, column, rowSpan, columnSpan;
+    ui->gridLayout->getItemPosition(1, &row, &column, &rowSpan, &columnSpan);
+    qDebug("index: 1, row: %d, column: %d, rowSpan: %d, columnSpan: %d",
+           row, column, rowSpan, columnSpan);
 }
 
 Widget::~Widget()
