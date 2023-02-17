f16fb3473c1fac4c9eac26dde22713e798e58ab8
vistar <1055311345@qq.com>
2023-02-17 23:27:56 +0800

    QFormLayout code

--- code/3.Layouts/3.formLayout/formLayout/widget.cpp
+++ code/3.Layouts/3.formLayout/formLayout/widget.cpp
@@ -6,6 +6,10 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    QPushButton *pushButton_3 = new QPushButton(ui->formLayoutWidget);
+
+    ui->formLayout->setWidget(3, QFormLayout::SpanningRole, pushButton_3);
 }
 
 Widget::~Widget()
